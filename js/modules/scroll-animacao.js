d767e098997a2daf3d0f144ba3e7871ca8b68b82
João Brosin <joao.brosin@gmail.com>
2021-07-15 20:45:09 -0300

    anima-numeros added

--- js/modules/scroll-animacao.js
+++ js/modules/scroll-animacao.js
@@ -7,10 +7,15 @@ export default function initAnimacaoScroll() {
       sections.forEach((section) => {
         const sectionTop = section.getBoundingClientRect().top;
         const isSectionVisible = (sectionTop - windowMetade) < 0;
-        if(isSectionVisible)
+        
+        if(isSectionVisible) {
           section.classList.add('ativo');
-        else 
+
+        } else if (section.classList.contains('ativo')) {
           section.classList.remove('ativo');
+
+        } 
+         
       })
     }
 
