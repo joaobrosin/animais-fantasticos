bc7d96b4d6a995fdd517805c40c917435f27ded7
joaobrosin <joao.brosin@gmail.com>
2021-08-05 17:37:14 -0300

    fetch 1 class added

--- modulos/06-javascript-assincrono/0603-fetch/script.js
+++ modulos/06-javascript-assincrono/0603-fetch/script.js
@@ -1,1 +1,55 @@
-console.log('aula 0603 Fetch');
+/**
+ * fetch doc.txt
+ */
+// const doc = fetch('./doc.txt');
+
+// doc
+// .then(resolucao => resolucao.text())
+// .then(body => {
+// console.log(body)
+// })
+
+/**
+ * fetch API cep
+ */
+// const cep = fetch('https://viacep.com.br/ws/81250200/json/');
+
+// cep
+// .then(resolucao => resolucao.json())
+// .then(body => {
+//   const conteudo = document.querySelector('.conteudo');
+
+//   conteudo.innerText = body.logradouro
+
+// })
+
+/**
+ * fetch CSS
+ */
+// const css = fetch('./style.css');
+
+// css
+// .then(resolucao => resolucao.text())
+// .then(body => {
+//   const conteudo = document.querySelector('.conteudo');
+//   const style = document.createElement('style');
+
+//   style.innerHTML = body;
+//   conteudo.appendChild(style);
+
+// })
+
+
+/**
+ * fetch página sobre
+ */
+const sobre = fetch('./sobre.html');
+const div = document.createElement('div');
+
+sobre
+.then(resolucao => resolucao.text())
+.then(body => {
+
+  div.innerHTML = body;
+  console.log(div)
+})
