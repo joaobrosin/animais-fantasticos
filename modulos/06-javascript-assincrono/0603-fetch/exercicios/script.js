5cabb322f0be20f704151e31d877e9a54f4ebec3
joaobrosin <joao.brosin@gmail.com>
2021-08-09 18:03:15 -0300

    working on fetch exercises

---
+++ modulos/06-javascript-assincrono/0603-fetch/exercicios/script.js
@@ -0,0 +1,32 @@
+/**
+ * Utilizando a API https://viacep.com.br/ws/${CEP}/json/
+ * crie um formulário onde o usuário pode digitar o cep
+ * e o endereço completo é retornado ao clicar em buscar
+ */
+const cepInput = document.querySelector('#cep');
+cepInput.addEventListener('change', () => {
+  buscaCep(cepInput.value);
+})
+
+function buscaCep (cep) {
+  fetch(`https://viacep.com.br/ws/${cep}/json/`)
+  .then(resolucao => resolucao.json())
+  .then((body) => {
+    document.querySelector('#logradouro').value = body.logradouro;
+    document.querySelector('#bairro').value = body.bairro;
+    document.querySelector('#cidade').value = body.localidade;
+    document.querySelector('#uf').value = body.uf;
+  })
+}
+
+/**
+ * Utilizando a API https://blockchain.info/ticker
+ * retorno no DOM o valor de compra da bitcoin em reais.
+ * Atualize este valor a cada 30 segundos
+ */
+
+/**
+ * Utilizando a API https://api.chucknorris.io/jokes/random
+ * retorne uma piada randomica do chucknorris, toda vez que
+ * clicar em próxima
+ */
--- modulos/06-javascript-assincrono/0603-fetch/script.js
+++ modulos/06-javascript-assincrono/0603-fetch/script.js
@@ -43,13 +43,70 @@
 /**
  * fetch página sobre
  */
-const sobre = fetch('./sobre.html');
-const div = document.createElement('div');
+// const sobre = fetch('./sobre.html');
+// const div = document.createElement('div');
 
-sobre
-.then(resolucao => resolucao.text())
-.then(body => {
+// sobre
+// .then(resolucao => resolucao.text())
+// .then(body => {
 
-  div.innerHTML = body;
-  console.log(div)
-})
+//   div.innerHTML = body;
+//   console.log(div)
+// })
+
+
+/**
+ * fetch de imagem
+ */
+// const imagem = fetch('./imagem.png');
+
+// imagem
+// .then(resolucao => resolucao.blob())
+// .then(body => {
+//   const blobUrl = URL.createObjectURL(body);
+//   const imagemDom = document.querySelector('img');
+//   imagemDom.src = blobUrl;
+// })
+
+
+/**
+ * json and text na mesma requisição
+ */
+// const cep = fetch('https://viacep.com.br/ws/81250200/json/');
+
+// cep
+// .then(resolucao => {
+//   const resolucao2 = resolucao.clone();
+  
+//   resolucao.text().then((text) => {
+//     console.log(text)
+//   })
+//   resolucao2.json().then((json) => {
+//     console.log(json)
+//   })
+
+//   console.log(resolucao)
+
+// })
+// .then(body => {
+//   console.log(body)
+// })
+
+/**
+ * pegando headers e status
+ */
+// const cep = fetch('https://viacep.com.br/ws/81250200/json/');
+
+// cep.then(response => {
+//   response.headers.forEach(console.log)
+//   console.log(response.status)
+// })
+
+/**
+ * cors
+ */
+// const link = fetch('https://www.google.com');
+
+// link.then(response => {
+//   console.log(response)
+// })
