d41b5bbf13b35245b5d93c70b1118158e1f137e4
João Brosin <joao.brosin@gmail.com>
2021-12-21 20:40:26 -0300

    refactoring adjustments added

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -34,7 +34,7 @@ export default class Modal {
   addModalEvents() {
     this.botaoAbrir.addEventListener("click", this.eventToggleModal);
     this.botaoFechar.addEventListener("click", this.eventToggleModal);
-    this.containerModal.addEventListener("click", this.eventToggleModal);
+    this.containerModal.addEventListener("click", this.cliqueForaModal);
   }
 
   init() {
